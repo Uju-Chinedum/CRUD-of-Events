2f0bf6d9a8d156904f6c83a692584fa7736e286f
Savadow <ujuchimaraoke@gmail.com>
2023-08-23 09:32:16 +0100

    feat: setup errorHandler and notFound middlewares and connection to database

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const connectDB = require("./db/connect");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
