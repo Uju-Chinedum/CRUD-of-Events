2c2a786a6a82352287d9a9450c67f4830a011144
Savadow <ujuchimaraoke@gmail.com>
2023-08-31 17:01:54 +0100

    feat: finished up

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/notFound");
 const errorHandler = require("./middleware/errorHandler");
@@ -11,7 +16,20 @@ const eventRouter = require("./routes/eventRoute");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+
+app.use(helmet());
+app.use(xss());
+app.use(cors());
+app.use(mongoSanitize());
 app.use(express.json());
+app.use(express.static("./public"));
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/events", authenticateUser, eventRouter);
